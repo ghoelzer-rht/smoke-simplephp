fe70a70a474a47dcd22aff88fddafa82c7f6fac6
Greg Hoelzer <ghoelzer@redhat.com>
2015-09-23 16:26:08 -0500

    continue conversion

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $my_database = $_ENV['MYSQL_DATABASE'];
 $mysql_service_host = $_ENV['MYSQL_SERVICE_HOST'];
 $mysql_service_port = $_ENV['MYSQL_SERVICE_PORT'];
 
-echo 'Connecting User: ' + $mysql_user . '/' + $mysql_password . ' DB: ' . $my_database . '@' . $mysql_service_host . ':' . $mysql_service_port;
+echo 'Connecting User: ' . $mysql_user . '/' . $mysql_password . ' DB: ' . $my_database . '@' . $mysql_service_host . ':' . $mysql_service_port;
 
 $mysql_host = $mysql_service_host . ":" . $mysql_service_port;
 
@@ -32,7 +32,9 @@ $result = $mysqli->query($query) or die('Query failed: ' . $mysqli->error);
 
 // Printing results in HTML
 echo "<table>\n";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+$result->data_seek(0);
+//while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+while ($line = $result->fetch_assoc()) {
 echo "\t<tr>\n";
 foreach ($line as $col_value) {
 echo "\t\t<td>$col_value</td>\n";
@@ -42,9 +44,9 @@ echo "\t</tr>\n";
 echo "</table>\n";
 
 // Free resultset
-mysql_free_result($result);
+mysqli_free_result($result);
 
 // Closing connection
-mysql_close($link);
+mysqli_close($link);
 ?>
 
