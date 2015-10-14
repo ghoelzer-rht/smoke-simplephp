8ead9da830a4b16c8ca9a274d981acde59141075
Greg Hoelzer <ghoelzer@redhat.com>
2015-10-14 15:03:43 -0500

    Testing pre-push hook

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.001';
+echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.002';
 echo "<br />";
 
 // List OpenShift Env Variables
