69af62e3e0eb4a953b7fb3fc09d0f6cdef823f9b
Greg Hoelzer <ghoelzer@redhat.com>
2015-10-15 17:06:26 -0500

    Change back to version 1 for testing of releases

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.004';
+echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.001';
 echo "<br />";
 
 // List OpenShift Env Variables
