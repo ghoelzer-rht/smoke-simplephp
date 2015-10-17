90aee583e518234cf9f798086b3de638c171234a
Greg Hoelzer <ghoelzer@redhat.com>
2015-10-17 17:31:04 -0500

    Testing manual deployment

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.0038';
+echo 'Welcome to the OpenShift 3 Simple PHP and MySQL Smoke DevOps Sample Application v1.0043';
 echo "<br />";
 
 // List OpenShift Env Variables
