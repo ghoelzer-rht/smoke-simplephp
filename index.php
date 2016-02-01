a39b78bd1747cecd832ff23d6aa42d9ce036eef8
Greg Hoelzer <ghoelzer@redhat.com>
2016-02-01 15:28:51 -0600

    Updated version to 1.0082

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">ver 1.0081</a></li>
+            <li class="active"><a href="#">ver 1.0082</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
